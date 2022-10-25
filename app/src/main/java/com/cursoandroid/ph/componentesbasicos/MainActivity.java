123f42546d73acea92925ea14bded69e65c25ad3
Paulo Henrique <34384560+paulohssouza@users.noreply.github.com>
2022-10-25 15:32:47 -0300

    Captura de inputs do usuário com um botão.

--- app/src/main/java/com/cursoandroid/ph/componentesbasicos/MainActivity.java
+++ app/src/main/java/com/cursoandroid/ph/componentesbasicos/MainActivity.java
@@ -3,12 +3,30 @@ package com.cursoandroid.ph.componentesbasicos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText inputName;
+    private EditText inputEmail;
+    private TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inputName = findViewById(R.id.inputName);
+        inputEmail = findViewById(R.id.inputEmail);
+        result = findViewById(R.id.result);
+    }
+
+    public void send (View view) {
+        String nome = inputName.getText().toString();
+        String email = inputEmail.getText().toString();
+        result.setText("Nome: " + nome + "\nEmail: " + email);
+    }
+
+    public void limpar () {
+
     }
 }
